e089722cbe63cd7c8f563e6015bc39e6eca8c43b
Pavel Golman <pavel.golman@postindustria.com>
2017-11-04 09:48:30 +0200

    test build

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import UserReducer from './reducer_users';
-import { reducer as formReducer } from 'redux-form';
-
-const rootReducer = combineReducers({
-  user: UserReducer
-});
-
-export default rootReducer;
